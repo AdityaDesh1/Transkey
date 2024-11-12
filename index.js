c9472aa152f28a5df59a35620689627026a54f4e
Aditya Deshpande <adityadeshpande1999@gmail.com>
2024-11-13 01:02:02 +0530

    Save unstaged changes

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use('/file', fileRoutes);
 
 
 // Start server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
